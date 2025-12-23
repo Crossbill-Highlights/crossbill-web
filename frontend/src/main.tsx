b0651185255cddcb5a34c8359d7e1c709da8fdad
Tuomas Salmi <salmi.tuomas@gmail.com>
2025-12-23 18:20:55 +0200

    Remove devtools

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { configureApiClient } from './api/config';
@@ -19,7 +18,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <CssBaseline />
         <App />
       </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
