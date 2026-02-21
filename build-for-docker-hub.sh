1b90478209c88297c0a447944c420eb80628d11c
Tuomas Salmi <salmi.tuomas@gmail.com>
2026-02-21 16:45:24 +0200

    Add timestamp to nightly builds
    Because Railway does not seem to trigger redeploy if the tag hasn't
    changed.

--- build-for-docker-hub.sh
+++ build-for-docker-hub.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 # Usage: ./build-for-docker-hub.sh
-# Builds and pushes nightly release to Docker Hub
+# Builds and pushes nightly release to Docker Hub with a unique tag
 # Stable releases should be done by CI
 
 set -e # Exit on error
 
-echo "Building nightly release..."
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+UNIQUE_TAG="nightly-${TIMESTAMP}"
+
+echo "Building nightly release (${UNIQUE_TAG})..."
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 docker build -t crossbill:nightly -f ./Dockerfile .
 docker tag crossbill:nightly tumetsu/crossbill:nightly
+docker tag crossbill:nightly tumetsu/crossbill:${UNIQUE_TAG}
 
 echo "Pushing to docker registry..."
 docker push tumetsu/crossbill:nightly
+docker push tumetsu/crossbill:${UNIQUE_TAG}
 
-echo "Done! Pushed tag: nightly"
+echo "Pushed tags: nightly, ${UNIQUE_TAG}"
